f614db60e11e556004d06bb890f702312ae5a607
Vincent Potucek <vpotucek@me.com>
2025-09-09 09:41:02 +0200

    fix opmd
    Signed-off-by: Vincent Potucek <vpotucek@me.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,8 +8,8 @@ plugins {
 
     id("gradlebuild.update-versions")            // Local development: Convenience tasks to update versions in this build: 'released-versions.json', 'agp-versions.properties', ...
     id("gradlebuild.wrapper")                    // Local development: Convenience tasks to update the wrapper (like 'nightlyWrapper')
-    id("com.diffplug.spotless") version "7.2.1"
-    id("org.openrewrite.rewrite") version "7.15.0"
+    id("com.diffplug.spotless") version "7.2.1" appply false
+    id("org.openrewrite.rewrite") version "7.15.0" appply false
 }
 
 description = "Adaptable, fast automation for all"
