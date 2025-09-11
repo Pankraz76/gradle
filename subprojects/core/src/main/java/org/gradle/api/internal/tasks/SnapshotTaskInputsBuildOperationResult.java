5e1974e8ae1604326407693a316c4bfa34600ebd
Vincent Potucek <vpotucek@me.com>
2025-09-11 20:15:36 +0200

    Fix `IdentityHashMapUsage`
    Signed-off-by: Vincent Potucek <vpotucek@me.com>

--- subprojects/core/src/main/java/org/gradle/api/internal/tasks/SnapshotTaskInputsBuildOperationResult.java
+++ subprojects/core/src/main/java/org/gradle/api/internal/tasks/SnapshotTaskInputsBuildOperationResult.java
@@ -69,9 +69,10 @@ public class SnapshotTaskInputsBuildOperationResult extends BaseSnapshotInputsBu
 
     private static class FilePropertyCollectingVisitor extends BaseFilePropertyCollectingVisitor<VisitState> implements InputFilePropertyVisitor {
 
-        @SuppressWarnings("deprecation")
+        @SuppressWarnings({"deprecation", "unused"})
         @Override
         protected Property createProperty(VisitState state) {
+            state.getPropertyNormalizationStrategyName();
             return new TaskProperty(HashCode.fromBytes(state.getPropertyHashBytes()).toString(), state.getPropertyAttributes());
         }
 
