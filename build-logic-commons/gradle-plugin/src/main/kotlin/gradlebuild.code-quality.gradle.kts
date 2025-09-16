4a532475d70be0a337bafc03b231dcc6e48ffd5e
Vincent Potucek <vpotucek@me.com>
2025-09-16 20:30:46 +0200

    Fix `ModifyCollectionInEnhancedForLoop`
    Signed-off-by: Vincent Potucek <vpotucek@me.com>

--- build-logic-commons/gradle-plugin/src/main/kotlin/gradlebuild.code-quality.gradle.kts
+++ build-logic-commons/gradle-plugin/src/main/kotlin/gradlebuild.code-quality.gradle.kts
@@ -147,10 +147,10 @@ tasks.withType<JavaCompile>().configureEach {
         disableWarningsInGeneratedCode = true
         allErrorsAsWarnings = true
         errorproneArgs.addAll(
-            "-XepPatchChecks:UnusedMethod",
+            "-XepPatchChecks:UnusedMethod -XepPatchLocation:IN_PLACE",
 //            "-XepPatchChecks:UndefinedEquals",
 //            "-XepPatchChecks:MissingOverride",
-            "-XepPatchLocation:IN_PLACE"
+//            "-XepPatchLocation:IN_PLACE"
         )
     }
 }
