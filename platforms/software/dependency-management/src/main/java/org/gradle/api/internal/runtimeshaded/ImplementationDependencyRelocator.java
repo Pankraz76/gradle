93e5a598e12b3884fc1a16a025dae62a33c06b71
Vincent Potucek <vpotucek@me.com>
2025-12-01 10:33:41 +0100

    [prone] fix `UTF_8`
    Signed-off-by: Vincent Potucek <vpotucek@me.com>

--- platforms/software/dependency-management/src/main/java/org/gradle/api/internal/runtimeshaded/ImplementationDependencyRelocator.java
+++ platforms/software/dependency-management/src/main/java/org/gradle/api/internal/runtimeshaded/ImplementationDependencyRelocator.java
@@ -25,7 +25,6 @@ import org.objectweb.asm.commons.Remapper;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.nio.charset.Charset;
 import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Matcher;
